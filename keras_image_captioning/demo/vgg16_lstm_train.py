40dbcb8ed7e6b01ec68f50ab4146bab271c959fe
xschen <xs0040@gmail.com>
2018-02-03 10:38:05 +0800

    implement the vgg16_lstm train script

---
+++ keras_image_captioning/demo/vgg16_lstm_train.py
@@ -0,0 +1,27 @@
+from keras_image_captioning.library.img_cap_loader import load_img_cap
+from keras_image_captioning.library.text_fit import fit_text
+from keras_image_captioning.library.vgg16_lstm import Vgg16LstmImgCap
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+def main():
+    seed = 42
+    max_vocab_size = 5000
+
+    np.random.seed(seed)
+    img_dir_path = './data/pokemon/img'
+    txt_dir_path = './data/pokemon/txt'
+    model_dir_path = './models/pokemon'
+    data = load_img_cap(img_dir_path, txt_dir_path)
+
+    train_data, test_data = train_test_split(data, test_size=0.2, random_state=seed)
+
+    config = fit_text(data, max_vocab_size=max_vocab_size)
+
+    img_cap = Vgg16LstmImgCap()
+    img_cap.fit(config, train_data, test_data, model_dir_path=model_dir_path)
+
+
+if __name__ == '__main__':
+    main()
